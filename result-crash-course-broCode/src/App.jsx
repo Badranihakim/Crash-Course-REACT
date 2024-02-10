fa6b170c6d6318977d2f600032c516014bd35f0c
Hakim Badrani <hakimbadrani05@gmail.com>
2024-02-10 08:20:36 +0700

    first commit

---
+++ result-crash-course-broCode/src/App.jsx
@@ -0,0 +1,9 @@
+import Button from "./sesi 3 styling/Button"
+
+export default function App(){
+  return (
+    <>
+      <Button />
+    </>
+  )
+}
---
+++ result-crash-course-broCode/src/sesi 3 styling/Button.jsx
@@ -0,0 +1,14 @@
+// How to style REACT components with CSS
+// Not including external frameworks or preprocessors
+
+// 1. EXTERNAL
+// 2. MODULES
+// 3. INTERNAL
+
+export default function Button(){
+    return(
+        <>
+        <button className="button">Click me</button>
+        </>
+    )
+}
